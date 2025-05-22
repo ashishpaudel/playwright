cbffb71cd2a0f89361e11c7eaffc256d84a27577
Aashish Poudel <aashish.poudel@ime.com.np>
2025-05-22 12:15:25 +0545

    comment

--- pom/homePage.js
+++ pom/homePage.js
@@ -5,10 +5,9 @@ export class Home{
         this.page = page
         this.click_search = this.page.locator('#q').fill('iphone 16 pro max')
         this.search = this.page.locator('#a2a0e.searchlist.search.d_go').click()
-
+        this.search = this.page.locator('#a2a0e.searchlist.search.d_go').click()
+        //kuheliuygie
     }
-
-    
     async home() {
         await this.click_search
         await this.search
