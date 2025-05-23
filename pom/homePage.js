5e5cb3101387f8568ae4f09ca614f4737ba489a6
Aashish Poudel <aashish.poudel@ime.com.np>
2025-05-23 17:27:03 +0545

    hshhdh

--- pom/homePage.js
+++ pom/homePage.js
@@ -3,14 +3,36 @@ import{test, expect } from '@playwright/test'
 export class Home{
     constructor(page){
         this.page = page
-        this.click_search = this.page.locator('#q').fill('iphone 16 pro max')
-        this.search = this.page.locator('#a2a0e.searchlist.search.d_go').click()
-        this.search = this.page.locator('#a2a0e.searchlist.search.d_go').click()
-        //kuheliuygie
-    }
-    async home() {
-        await this.click_search
-        await this.search
+        this.click_search =this.page.getByPlaceholder('Search in Daraz')  //,{waitUntil: 'domcontentloaded'}
+        this.search = this.page.locator('.search-box__button--1oH7')
+        this.productList = this.page.locator('.WNoq3')
+        this.addToCartBtn =this.page.getByRole('button',{name:'Add to Cart'})
+        this.goCart = this.page.locator('.cart-icon-daraz') 
         
+    }
+    async home(iphone) {
+       
+        await this.click_search.fill(iphone)
+        
+        await this.search.click()
+         await this.page.waitForTimeout(1000)
+    }
+    async addToCart(productName){
+        // const allProduct = await (this.productList)
+        for (const product of (await (this.productList))){
+            if (productName === await product.textContent()) {
+                await product.click()
+                break;
+            }
+        }
+        await this.page.on('dialog', async dialog =>{
+            if(dialog.message().includes('added')){
+                await dialog.accept()
+            }
+        })
+        await this.addToCartBtn.click()
+    }
+    async gotoCart(){
+        await this.goCart.click()
     }
 }
--- tests/daraz.spec.js
+++ tests/daraz.spec.js
@@ -1,6 +1,6 @@
 import {expect, test} from '@playwright/test'
-import {login} from '../mop/login'
-import {Home, homePage} from '../mop/homePage'
+import {login} from '../pom/login'
+import {Home, homePage} from '../pom/homePage'
 
 test('Daraz',async({page}) =>{ 
     //login page  
@@ -9,6 +9,8 @@ test('Daraz',async({page}) =>{
     await Login.login_page('ashishpoudel.2015@gmail.com' , 'Admin@123' )
     
     //home page 
-    const HomePage = new Home()
-    await HomePage.home(page)
+    const HomePage = new Home(page)
+    await HomePage.home('iphone')
+    await HomePage.addToCart('Apple iPhone 14 (128GB) 6.1&quot; Genuine Super Retina OLED Display')
+    await HomePage.gotoCart()
 })
